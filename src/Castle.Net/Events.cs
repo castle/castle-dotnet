2e0c50f1e508d92e3408f804fe8efe46deeaa002
Magnus Nilsson <magnus@tretton37.com>
2019-03-08 10:04:02 +0100

    Constants for events and header whitelist

---
+++ src/Castle.Net/Events.cs
@@ -0,0 +1,22 @@
+﻿namespace Castle
+{
+    public static class Events
+    {
+        public const string LoginFailed = "$login.failed";
+        public const string LogoutSucceeded = "$logout.succeeded ";
+        public const string ProfileUpdateSucceeded = "$profile_update.succeeded ";
+        public const string ProfileUpdateFailed = "$profile_update.failed ";
+        public const string RegistrationSucceeded = "$registration.succeeded  ";
+        public const string RegistrationFailed = "$registration.failed ";
+        public const string PasswordResetSucceeded = "$password_reset.succeeded";
+        public const string PasswordResetFailed = "$password_reset.failed ";
+        public const string PasswordResetRequestSucceeded = "$password_reset_request.succeeded";
+        public const string PasswordResetRequestFailed = "$password_reset_request.failed";
+        public const string IncidentMitigated = "$incident.mitigated";
+        public const string ReviewEscalated = "$review.escalated";
+        public const string ReviewResolved = "$review.resolved";
+        public const string ChallengeRequested = "$challenge.requested";
+        public const string ChallengeSucceeded = "$challenge.succeeded";
+        public const string ChallengeFailed = "$challenge.failed";
+    }
+}
---
+++ src/Castle.Net/Headers.cs
@@ -0,0 +1,24 @@
+﻿namespace Castle
+{
+    public static class Headers
+    {
+        public static readonly string[] Whitelist = new[]
+        {
+            "Accept",
+            "Accept-Charset",
+            "Accept-Datetime",
+            "Accept-Encoding",
+            "Accept-Language",
+            "Cache-Control",
+            "Connection",
+            "Content-Length",
+            "Content-Type",
+            "Cookie",
+            "Host",
+            "Origin",
+            "Pragma",
+            "Referer",
+            "User-Agent",
+        };
+    }
+}
