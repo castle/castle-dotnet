59a58d727db7fe5e8322b7e7908a87bd387bc88d
Magnus Nilsson <magnus@tretton37.com>
2019-03-11 13:51:18 +0100

    Helper for computing HMAC signature

---
+++ src/Castle.Net/Signature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Castle
+{
+    public static class Signature
+    {
+        public static string Compute(string key, string message)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+
+            using (var hmac = new HMACSHA256(keyBytes))
+            {
+                var hashed = hmac.ComputeHash(messageBytes);
+                return string.Concat(Array.ConvertAll(hashed, x => x.ToString("x2")));
+            }
+        }
+    }
+}
---
+++ src/Tests/When_computing_signature.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AutoFixture.Xunit2;
+using Xunit;
+
+namespace Tests
+{
+    public class When_computing_signature
+    {
+        [Theory, AutoData]
+        public void Should_create_valid_hex_string(string key, string message)
+        {
+            var result = Castle.Signature.Compute(key, message);
+
+            var numberGroups = Regex.Matches(result, @".{2}");
+            var numbers = numberGroups.Select(s => int.Parse(s.Value, NumberStyles.HexNumber)).ToList();
+        }
+    }
+}
