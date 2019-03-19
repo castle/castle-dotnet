e4d44915066eafa8b55ded04841fb72ace774680
Magnus Nilsson <magnus@tretton37.com>
2019-03-19 17:21:50 +0100

    Can get ip from http header when building context

--- src/Castle.Sdk/Context.cs
+++ src/Castle.Sdk/Context.cs
@@ -8,7 +8,7 @@ namespace Castle
     public static class Context
     {
 #if NET461
-        public static RequestContext FromHttpRequest(System.Web.HttpRequestBase request)
+        public static RequestContext FromHttpRequest(System.Web.HttpRequestBase request, string ipHeader = null)
         {
             var headers = new Dictionary<string, string>();
             foreach (string key in request.Headers.Keys)
@@ -18,25 +18,29 @@ namespace Castle
 
             var clientId = request.Headers.AllKeys.Contains("X-Castle-Client-ID", StringComparer.OrdinalIgnoreCase)
                 ? request.Headers["X-Castle-Client-ID"]
-                : request.Cookies["__cid"]?.Value;
+                : request.Cookies["__cid"]?.Value ?? "";
+
+            var ip = ipHeader != null && request.Headers.AllKeys.Contains(ipHeader, StringComparer.OrdinalIgnoreCase)
+                ? request.Headers[ipHeader]
+                : request.UserHostAddress;
 
             return new RequestContext()
             {
                 ClientId = clientId,
                 Headers = headers,
-                Ip = request.UserHostAddress
+                Ip = ip
             };
         }
 #endif
 
 #if NETSTANDARD2_0
-        public static RequestContext FromHttpRequest(Microsoft.AspNetCore.Http.HttpRequest request)
+        public static RequestContext FromHttpRequest(Microsoft.AspNetCore.Http.HttpRequest request, string ipHeader = null)
         {
             return new RequestContext()
             {
                 ClientId = GetClientId(request.Headers, request.Cookies),
                 Headers = request.Headers.ToDictionary(x => x.Key, y => y.Value.FirstOrDefault()),
-                Ip = request.HttpContext.Connection.RemoteIpAddress.ToString(),
+                Ip = GetIp(() => request.HttpContext.Connection.RemoteIpAddress.ToString(), request.Headers, ipHeader)
             };
         }
 
@@ -46,7 +50,17 @@ namespace Castle
         {
             return headers.TryGetValue("X-Castle-Client-ID", out var headerId)
                 ? headerId.First()
-                : cookies["__cid"];
+                : cookies["__cid"] ?? "";
+        }
+
+        internal static string GetIp(
+            Func<string> getIpFromHttpContext,
+            IDictionary<string, Microsoft.Extensions.Primitives.StringValues> headers,
+            string ipHeader)
+        {
+            return ipHeader != null && headers.TryGetValue(ipHeader, out var headerValues) 
+                ? headerValues.First() 
+                : getIpFromHttpContext();
         }
 #endif
     }
--- src/Tests/When_creating_request_context.cs
+++ src/Tests/When_creating_request_context.cs
@@ -13,14 +13,11 @@ namespace Tests
         [Theory, AutoData]
         public void Should_get_client_id_from_castle_header_if_present(
             string castleHeaderValue,
-            string otherHeader,
-            string otherHeaderValue,
             string cookieValue)
         {
             var headers = new Dictionary<string, StringValues>()
             {
                 ["X-Castle-Client-ID"] = castleHeaderValue,
-                [otherHeader] = otherHeaderValue
             };
 
             var cookies = new RequestCookieCollection(new Dictionary<string, string>()
@@ -31,6 +28,87 @@ namespace Tests
             var result = Context.GetClientId(headers, cookies);
 
             result.Should().Be(castleHeaderValue);
+        }
+
+        [Theory, AutoData]
+        public void Should_get_client_id_from_cookie_if_castle_header_not_present(
+            string otherHeader,
+            string otherHeaderValue,
+            string cookieValue)
+        {
+            var headers = new Dictionary<string, StringValues>()
+            {
+                [otherHeader] = otherHeaderValue
+            };
+
+            var cookies = new RequestCookieCollection(new Dictionary<string, string>()
+            {
+                ["__cid"] = cookieValue
+            });
+
+            var result = Context.GetClientId(headers, cookies);
+
+            result.Should().Be(cookieValue);
+        }
+
+        [Theory, AutoData]
+        public void Should_use_empty_string_if_unable_to_get_client_id(
+            string otherHeader,
+            string otherHeaderValue,
+            string otherCookie,
+            string otherCookieValue)
+        {
+            var headers = new Dictionary<string, StringValues>()
+            {
+                [otherHeader] = otherHeaderValue
+            };
+
+            var cookies = new RequestCookieCollection(new Dictionary<string, string>()
+            {
+                [otherCookie] = otherCookieValue
+            });
+
+            var result = Context.GetClientId(headers, cookies);
+
+            result.Should().Be("");
+        }
+
+        [Theory, AutoData]
+        public void Should_get_ip_from_supplied_header(
+            string ipHeader,
+            string ip,
+            string otherHeader,
+            string otherHeaderValue,
+            string httpContextIp)
+        {
+            var headers = new Dictionary<string, StringValues>()
+            {
+                [ipHeader] = ip,
+                [otherHeader] = otherHeaderValue
+            };
+
+            var result = Context.GetIp(() => httpContextIp, headers, ipHeader);
+
+            result.Should().Be(ip);
+        }
+
+        [Theory, AutoData]
+        public void Should_get_ip_from_httpcontext_if_no_header_supplied(
+            string ipHeader,
+            string ip,
+            string otherHeader,
+            string otherHeaderValue,
+            string httpContextIp)
+        {
+            var headers = new Dictionary<string, StringValues>()
+            {
+                [ipHeader] = ip,
+                [otherHeader] = otherHeaderValue
+            };
+
+            var result = Context.GetIp(() => httpContextIp, headers, null);
+
+            result.Should().Be(httpContextIp);
         }
     }
 }
