f5af3c0ff2cfcfc417aeefd4fda4f1cb8a47f405
Magnus Nilsson <magnus@tretton37.com>
2019-03-28 09:42:08 +0100

    Streamlined action signature func calls

--- src/Tests/Actions/When_tracking.cs
+++ src/Tests/Actions/When_tracking.cs
@@ -3,7 +3,6 @@ using Castle.Actions;
 using Castle.Config;
 using Castle.Messages.Responses;
 using FluentAssertions;
-using Newtonsoft.Json.Linq;
 using Tests.SetUp;
 using Xunit;
 
@@ -13,13 +12,12 @@ namespace Tests
     {
         [Theory, AutoFakeData]
         public async Task Should_return_response_if_successful(
-            JObject request,
             CastleConfiguration configuration)
         {
             var response = new VoidResponse();
-            Task<VoidResponse> Send(JObject req) => Task.FromResult(response);
+            Task<VoidResponse> Send() => Task.FromResult(response);
 
-            var result = await Track.Execute(Send, request, configuration);
+            var result = await Track.Execute(Send, configuration);
 
             result.Should().Be(response);
         }
